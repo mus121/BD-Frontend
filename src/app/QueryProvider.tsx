fc6499d83295e2ec31e27932d4b5763b67697421
mus121 <daniyalkamal56@gmail.com>
2025-03-12 07:57:52 +0500

    DevTools Issue Fixed

--- src/app/QueryProvider.tsx
+++ src/app/QueryProvider.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 // eslint-disable-next-line import/no-extraneous-dependencies
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient({
@@ -12,12 +12,14 @@ const queryClient = new QueryClient({
     },
   },
 });
+
 function QueryProvider({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      {process.env.NODE_ENV !== 'production' && <ReactQueryDevtools initialIsOpen={false} />}
       {children}
     </QueryClientProvider>
   );
 }
+
 export default QueryProvider;
